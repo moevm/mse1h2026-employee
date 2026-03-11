6189b8299fc1ecff39e785309d83bd84f06da4be
mor1la <mor1la@mail.ru>
2026-03-12 01:58:15 +0300

    feat: add bot and google sheets constants

---
+++ src/constants/bot_constants.py
@@ -0,0 +1,11 @@
+class Buttons:
+    AUTH = "Авторизация"
+    REQUEST_ROLE = "Получение роли"
+    BACK = "Назад"
+
+
+class Callbacks:
+    START_AUTH = "start_auth"
+    REQUEST_ROLE = "request_role"
+    BACK = "back_to_start"
+    AUTH_ROLE = "auth_role"
